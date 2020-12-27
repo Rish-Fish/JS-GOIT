f29eac053838e8c3087cefc752b54b12a953be19
Rish-Fish <rita.levkoy@gmail.com>
2020-12-27 14:06:10 +0200

    init commit

---
+++ js/try-callback-form.js
@@ -0,0 +1,24 @@
+// const callbackForm = document.querySelector('.callback-form-container');
+// const requestReceivedModal = document.querySelector('#request-received');
+
+// const userName = document.querySelector('#callback-form-input-name');
+// const userEmail = document.querySelector('#callback-form-input-email');
+// const userPhone = document.querySelector('#callback-form-input-phone');
+
+
+// callbackForm.addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   if (userName.value.trim())
+
+  
+
+
+
+
+
+//   requestReceivedModal.classList.add('modal-active');
+//   setTimeout(function() {
+//     requestReceivedModal.classList.remove('modal-active');
+//   }, 2000);
+// });
